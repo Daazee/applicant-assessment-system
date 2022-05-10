2db574a70428ed8f3d6c52e27f72351a50cde673
Azeez Adebayo <azeez.adedayo@yahoo.com>
2022-05-10 20:04:21 +0100

    added utility class

---
+++ ApplicantAssessmentSystem.App/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicantAssessmentSystem.App
+{
+    public class Utility
+    {
+        public static string HashPassword(string password)
+        {
+            string result;
+            SHA1Managed sha1 = new SHA1Managed();
+            byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder sb = new StringBuilder(hash.Length * 2);
+            foreach (byte b in hash)
+            {
+                sb.Append(b.ToString("x2"));
+            }
+            result = sb.ToString();
+            return result;
+        }
+    }
+}
